500fad108cdb90a2dcda19e69324c77927daf64e
Alaa Zorkane <alaazorkane@gmail.com>
2023-09-03 22:27:43 +0100

    add custom domain

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,10 @@
 import type { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
+type GOptions = Parameters<SSTConfig["config"]>[0];
+
 export default {
-  config(input) {
+  config(input: GOptions) {
     const name = "sunny";
     const stage = input.stage || "kid";
 
@@ -18,24 +20,11 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const isPreview = stack.stage === "kid";
-      const isStaging = stack.stage === "law";
-
-      let domain = "sunny";
-
-      // TODO: use a better random id or build hash
-      const randomId = Math.random().toString(36).slice(2, 12);
-
-      if (isPreview) {
-        domain = `kid-${domain}-${randomId}`;
-      } else if (isStaging) {
-        domain = `law-${domain}`;
-      }
-
       const site = new NextjsSite(stack, "site", {
         runtime: "nodejs18.x",
         edge: true,
-        path: "apps/web"
+        path: "apps/web",
+        customDomain: "test.acme.io",
       });
 
       stack.addOutputs({
